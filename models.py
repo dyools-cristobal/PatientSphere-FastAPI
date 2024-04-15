8853fde0cbc6c8a0c274b5a224288a2ab0f605b1
dyools-cristobal <78533777+dyools-cristobal@users.noreply.github.com>
2024-04-15 20:41:41 +0800

    refactored apis

--- models.py
+++ models.py
@@ -1,56 +1,74 @@
 from decimal import Decimal
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from sqlalchemy import Boolean, Column, Integer, String, Float, Date, Time
+from database import Base
 from typing import List, Optional
 
-class Patient(BaseModel):
-    id: int
-    first_name: str
-    middle_name: Optional[str]
-    last_name: str
-    photo_link: Optional[str]
-    
-    class PhysicalDetails(BaseModel):
-        gender: Optional[str]
-        age_of_gestation: Optional[Decimal]
-        birth_weight: Optional[Decimal]
-        birth_length: Optional[Decimal]
-        head_circumference: Optional[Decimal]
-        chest_circumference: Optional[Decimal]
-        abdomen_circumference: Optional[Decimal]
-        newborn_screening: Optional[str]
-        blood_type: Optional[str]
-        known_allergies: List[str]
-        perinatal_history: Optional[str]
-        date_of_birth: Optional[str]
-        time_of_birth: Optional[str]
+class User(Base):
+    __tablename__ = 'users'
 
-    class ParentDetails(BaseModel):
-        
-        class MotherDetails(BaseModel):
-            first_name: Optional[str]
-            last_name: Optional[str]
-            age: Optional[Decimal]
-            occupation: Optional[str]
-            contact_number: Optional[str]
-            email_address: Optional[str]
+    id = Column(Integer, primary_key=True, index=True)
+    role_id = Column(Integer)
+    clinic_id = Column(Integer)
+    username = Column(String(50), unique=True)
 
-        class FatherDetails(BaseModel):
-            first_name: Optional[str]
-            last_name: Optional[str]
-            age: Optional[Decimal]
-            occupation: Optional[str]
-            contact_number: Optional[str]
-            email_address: Optional[str]
+class Role(Base):
+    __tablename__ = 'roles'
 
-        class HomeDetails(BaseModel):
-            address: Optional[str]
-            home_address: Optional[str]
+    id = Column(Integer, primary_key=True, index=True)
+    description = Column(String(50))
 
-        mother_details: MotherDetails
-        father_details: FatherDetails
-        home_details: HomeDetails    
+class Clinic(Base):
+    __tablename__ = 'clinics'
 
-    physical_details: PhysicalDetails
-    parent_details: ParentDetails
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50))
+    address = Column(String(50))
+    phone_number = Column(String(50))
+
+class Patient(Base):
+    __tablename__ = 'patients'
+
+    id = Column(Integer, primary_key=True, index=True)
+    dr_id = Column(Integer)
+    first_name = Column(String(50))
+    middle_name = Column(String(50))
+    last_name = Column(String(50))
+    photo_link = Column(String(150))
+    gender = Column(Integer)
+    age_of_gestation = Column(Float)
+    birth_weight = Column(Float)
+    birth_length = Column(Float)
+    head_circumference = Column(Float)
+    chest_circumference = Column(Float)
+    abdomen_circumference = Column(Float)
+    newborn_screening = Column(String(250))
+    blood_type = Column(String(50))
+    known_allergies = Column(String(250))
+    perinatal_history = Column(String(250))
+    date_of_birth = Column(String(12))
+    time_of_birth = Column(String(12))
+    mother_first_name = Column(String(50))
+    mother_last_name = Column(String(50))
+    mother_age = Column(Integer)
+    mother_occupation = Column(String(50))
+    mother_contact_number = Column(String(50))
+    mother_email_address = Column(String(50))
+    father_first_name = Column(String(50))
+    father_last_name = Column(String(50))
+    father_age = Column(Integer)
+    father_occupation = Column(String(50))
+    father_contact_number = Column(String(50))
+    father_email_address = Column(String(50))
+    home_address = Column(String(150))
+    home_phone_number = Column(String(50))
+
+class Growth(Base):
+    __tablename__ = 'growth'
+
+    id = Column(Integer, primary_key=True, index=True)
+    patient_id = Column(Integer)
+    date_taken = Column(Date)
+    height = Column(Float)
+    weight = Column(Float)
